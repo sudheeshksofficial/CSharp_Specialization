c334da90bf401bcb63ff37575a5f4459ae2ad333
sudheeshksofficial <sudheeshksofficial@gmail.com>
2023-05-13 22:34:21 +0530

    Introduction to csharp added

---
+++ IntroCsharp/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using static System.Console;
+
+namespace IntroCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+
+            int number = int.Parse(ReadLine());
+
+            Console.WriteLine($" The value got inserted is {number} ");
+            Console.WriteLine(" The value got inserted is {0}",number);
+            Console.WriteLine(" The value got inserted is "+number);
+
+            WriteLine("Enter two strings to get concatenated !!!");
+
+            string s1 = ReadLine();
+            string s2 = ReadLine();
+            Console.WriteLine("the concatenated strings are = "+s1+s2);
+        }
+    }
+}
