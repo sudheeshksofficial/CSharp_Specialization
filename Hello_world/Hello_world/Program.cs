3db5fa12370cdf81253ee7371df72dc827a47b6c
sudheeshksofficial <sudheeshksofficial@gmail.com>
2023-05-13 16:02:17 +0530

    basic program uploaded

---
+++ Hello_world/Hello_world/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_world
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
